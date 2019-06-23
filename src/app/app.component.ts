aab74980d5be3b5e669cb615714284636a18e6be
ohtsu <ohtsu@digipub-net.com>
2019-06-23 23:25:32 +0900

    Add Config Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-// import { Component } from '@angular/core';
-// import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
-
 import { Component } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
+// import { Component } from '@angular/core';
 
 // @Component({selector: 'ngbd-carousel-basic', 
 // templateUrl: './carousel-basic.html'})
@@ -13,17 +13,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-  // providers: [NgbCarouselConfig] 
+  styleUrls: ['./app.component.scss'],
+  providers: [NgbCarouselConfig] 
 })
 export class AppComponent {
-  title = 'ng8-bootstrap4-carousel';
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  // title = 'ng8-bootstrap4-carousel';
+  // images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
-//   constructor(config: NgbCarouselConfig) {
-//     // customize default values of carousels used by this component tree
-//     config.interval = 2000;
-//     config.wrap = true;
-//     config.keyboard = true;
-//   }
+  images = [1, 2, 3, 4].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = true;
+  }
 }
