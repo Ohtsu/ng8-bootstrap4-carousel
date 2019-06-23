fd5d195051f06ca7b8102077df0fa3721e52337d
ohtsu <ohtsu@digipub-net.com>
2019-06-23 17:49:31 +0900

    First Upload

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var path = require('path');
+var minimist = require('minimist');
+var pathvar = path.parse(__dirname);
+var current_dir = pathvar.name;
+const SOLUTION_NAME = current_dir;
+var argv = minimist(process.argv.slice(2));
+
+var dateFormat = {
+  fmt : {
+    "yyyy": function(date) { return date.getFullYear() + ''; },
+    "MM": function(date) { return ('0' + (date.getMonth() + 1)).slice(-2); },
+    "dd": function(date) { return ('0' + date.getDate()).slice(-2); },
+    "hh": function(date) { return ('0' + date.getHours()).slice(-2); },
+    "mm": function(date) { return ('0' + date.getMinutes()).slice(-2); },
+    "ss": function(date) { return ('0' + date.getSeconds()).slice(-2); }
+  },
+  format:function dateFormat (date, format) {
+    var result = format;
+    for (var key in this.fmt)
+      result = result.replace(key, this.fmt[key](date));
+    return result;
+  }
+};
+
+var time_version = dateFormat.format(new Date(),'yyyyMMddhhmm');
+const message = argv['m'];
+var TARGET = "../"+SOLUTION_NAME+"_"+time_version;
+if (message != undefined) {
+    TARGET = TARGET  +"_"+message;
+}
+
+
+gulp.task('default',function(){
+  console.log('current_dir -------',current_dir);
+  console.log('time_version ------',time_version);
+  console.log('target_path  ------',TARGET);
+  console.log('message      ------',argv['m']);
+  return gulp
+	.src(['./**','./.**','!./node_modules/**'])
+	.pipe(gulp.dest(TARGET));
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+// import { Component } from '@angular/core';
+// import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
+import { Component } from '@angular/core';
+
+// @Component({selector: 'ngbd-carousel-basic', 
+// templateUrl: './carousel-basic.html'})
+// export class NgbdCarouselBasic {
+//   images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+// }
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+  // providers: [NgbCarouselConfig] 
+})
+export class AppComponent {
+  title = 'ng8-bootstrap4-carousel';
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+//   constructor(config: NgbCarouselConfig) {
+//     // customize default values of carousels used by this component tree
+//     config.interval = 2000;
+//     config.wrap = true;
+//     config.keyboard = true;
+//   }
+}
